a2b4c2771cb049924b7ad3b0d8d907c41864a65c
enfyna <ramazanaslan21901@gmail.com>
2024-08-18 02:47:50 +0300

    varsayılan nx sayfasını sil

--- apps/uniforms-web/src/app/app.component.ts
+++ apps/uniforms-web/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, AboutComponent],
+  imports: [RouterModule, HeaderComponent, AboutComponent],
   selector: 'uf-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/uniforms-web/src/app/app.routes.ts
+++ apps/uniforms-web/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { AboutComponent } from './about/about.component';
 
 export const appRoutes: Route[] = [
-    { path: 'nx', component: NxWelcomeComponent },
     { path: 'about', component: AboutComponent },
 ];
