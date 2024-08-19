319132d03c51ddc4d2c4d32867fc0185e001ba17
enfyna <ramazanaslan21901@gmail.com>
2024-08-19 23:08:38 +0300

    Header logosunu tıklanabilir yap, linkleri düzenle

--- apps/uniforms-web/src/app/app.component.ts
+++ apps/uniforms-web/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, AboutComponent],
+  imports: [RouterModule, HeaderComponent],
   selector: 'uf-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/uniforms-web/src/app/app.routes.ts
+++ apps/uniforms-web/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Route } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { TemplatesComponent } from './templates/templates.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 export const appRoutes: Route[] = [
     { path: '', component: MainPageComponent },
-    { path: 'about', component: AboutComponent },
     { path: 'templates', component: TemplatesComponent },
 ];
