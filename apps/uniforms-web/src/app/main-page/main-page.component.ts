213764f7df7e1b6b6a6770c73aefeee14f97bed6
enfyna <ramazanaslan21901@gmail.com>
2024-08-20 16:26:28 +0300

    Ana sayfa eksik kısımları ekle

--- apps/uniforms-web/src/app/main-page/main-page.component.ts
+++ apps/uniforms-web/src/app/main-page/main-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { UfIntroCardComponent } from '@uniforms/uf-web-components'
+import {
+  UfIntroCardComponent,
+  UfButtonComponent,
+} from '@uniforms/uf-web-components'
 
 @Component({
   selector: 'uf-main-page',
   standalone: true,
-  imports: [CommonModule, UfIntroCardComponent],
+  imports: [CommonModule, UfIntroCardComponent, UfButtonComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
