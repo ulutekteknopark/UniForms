ee94a0458bc9cfd45a478a318912e373c8b926b6
enfyna <ramazanaslan21901@gmail.com>
2024-08-20 16:27:22 +0300

    Footer ekle

--- apps/uniforms-web/src/app/app.component.ts
+++ apps/uniforms-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   selector: 'uf-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
