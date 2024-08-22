ab76bad33c41c847ea621c194f02f5869a10c953
KenanTasavli <kenantasavli380@hotmail.com>
2024-08-22 15:19:09 +0300

    firebase entegre completed.

---
+++ apps/uniforms-web/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from './environments/environment';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),  // Firebase'i burada başlatıyoruz
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ apps/uniforms-web/src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyCsK44cFOWAiXz3NN4E01IAV7b1lvPtlTk",
+      authDomain: "uniforms-deneme.firebaseapp.com",
+      projectId: "uniforms-deneme",
+      storageBucket: "uniforms-deneme.appspot.com",
+      messagingSenderId: "699949914080",
+      appId: "1:699949914080:web:251a470a28a63f41e4fd85"
+    }
+  };
+  
