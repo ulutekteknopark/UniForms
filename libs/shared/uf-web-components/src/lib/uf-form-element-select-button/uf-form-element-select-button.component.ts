46553732629e71258666eb3b9bebbc9a01ecf7bc
Ramazan Aslan <ramazanaslan21901@gmail.com>
2024-08-23 15:20:52 +0300

    Ripple efekti ekle

--- libs/shared/uf-web-components/src/lib/uf-form-element-select-button/uf-form-element-select-button.component.ts
+++ libs/shared/uf-web-components/src/lib/uf-form-element-select-button/uf-form-element-select-button.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { MatRipple } from '@angular/material/core';
 
 @Component({
   selector: 'uf-form-element-select-button',
   standalone: true,
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule, MatIcon, MatRipple],
   templateUrl: './uf-form-element-select-button.component.html',
   styleUrl: './uf-form-element-select-button.component.scss',
 })
