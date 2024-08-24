37001bb3721bb721adaf487c3bdd09b2399793fe
Ramazan Aslan <ramazanaslan21901@gmail.com>
2024-08-24 23:05:14 +0300

    Soruların sıralarını değiştirmek için buton ekle

--- apps/uniforms-web/src/app/form-editor/form-editor.component.ts
+++ apps/uniforms-web/src/app/form-editor/form-editor.component.ts
@@ -31,7 +31,15 @@ export class FormEditorComponent {
   addQuestion(type: string){
     const ref = this.formContainer.createComponent(UfFormQuestionComponent);
 
-    ref.setInput("id", Math.ceil(Math.random() * 10000));
+    ref.setInput("id", Math.ceil(Math.random() * 1000000));
+
+    var position = 0;
+    this.questions.forEach(elm => {
+      if (elm.instance.position >= position)
+        position = elm.instance.position + 1;
+    })
+    ref.setInput("position", position);
+
     ref.setInput("type", type);
     ref.setInput("editable", true);
 
@@ -40,19 +48,53 @@ export class FormEditorComponent {
       alert("edit:" + ref.instance.id);
     })
 
+    let changePositionListener = ref.instance.changePositionEvent.subscribe(direction => {
+      // 3. soru aşağı indirilirse 4. soru olur. 4. soru 3. soru olur.
+      // 3. soru yukarı çıkarılırsa 2. soru olur. 2. soru 3. soru olur.
+      var other_node_position = ref.instance.position + direction;
+      var node_arr = this.questions.filter(
+        elm => other_node_position == elm.instance.position
+      );
+
+      if (node_arr.length > 1)
+        throw new Error('Aynı pozisyonda birden fazla soru var');
+      if (node_arr.length == 0)
+        throw new Error('Değiştirilecek pozisyonda soru bulunamadı');
+
+      var node = node_arr[0];
+
+      node.instance.position = ref.instance.position;
+      ref.instance.position = other_node_position;
+
+      this.formContainer.move(ref.hostView, ref.instance.position);
+    })
+
     let deletionListener = ref.instance.deleteEvent.subscribe(()=>{
+      // silinen sorudan sonraki soruların pozisyonlarını düşür
+      // 2. soru silindiğinde 4. soru 3. soru olur
+      this.questions.forEach(elm => {
+        if (elm.instance.position >= ref.instance.position)
+          elm.instance.position -= 1;
+      })
+
+      // soruyu diziden çıkar
       this.questions = this.questions.
         filter((elm) => elm.instance.id != ref.instance.id);
 
+      // sorunun dinleyicilerini diziden çıkar
       this.listeners = this.listeners.
-        filter(elm => elm != deletionListener).
-        filter(elm => elm != editListener);
+        filter(elm => elm !== changePositionListener).
+        filter(elm => elm !== deletionListener).
+        filter(elm => elm !== editListener);
 
+      // temizle
+      changePositionListener.unsubscribe();
       deletionListener.unsubscribe();
+      editListener.unsubscribe();
       ref.destroy();
     })
 
-    this.listeners.push(deletionListener, editListener);
+    this.listeners.push(deletionListener, editListener, changePositionListener);
     this.questions.push(ref);
   }
 
--- libs/shared/uf-web-components/src/lib/uf-form-question/uf-form-question.component.ts
+++ libs/shared/uf-web-components/src/lib/uf-form-question/uf-form-question.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, output, OutputEmitterRef } from '@angular/core';
+import { Component, Input, Output, output, OutputEmitterRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFabButton } from '@angular/material/button';
+import { MatFabButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 
 import { UfFormInputComponent } from '../uf-form-input/uf-form-input.component';
@@ -10,29 +10,35 @@ import { UfFormRateInputComponent } from '../uf-rate-input/uf-form-rate-input.co
 @Component({
   selector: 'uf-form-question',
   standalone: true,
-  imports: [CommonModule, MatFabButton, MatIcon, UfFormRateInputComponent, UfFormInputComponent, UfTextareaComponent],
+  imports: [CommonModule, MatFabButton, MatIconButton, MatIcon, UfFormRateInputComponent, UfFormInputComponent, UfTextareaComponent],
   templateUrl: './uf-form-question.component.html',
   styleUrl: './uf-form-question.component.scss',
 })
 export class UfFormQuestionComponent {
   @Input() editable: boolean = false;
 
-  @Input() id: number = 0;
+  @Input() id: number = -1;
+  @Input() position: number = -1;
   @Input() image: string = "";
   @Input() question: string = "";
   @Input() type: string = "name";
   @Input() required: boolean = false;
 
+  @Output() direction: number = 0;
+
   deleteEvent: OutputEmitterRef<void> = output();
   editEvent: OutputEmitterRef<void> = output();
+  changePositionEvent: OutputEmitterRef<number> = output();
+
+  changePosition(direction: number){
+    this.changePositionEvent.emit(direction);
+  }
 
   edit(){
-    console.log("emit edit " + this.type);
     this.editEvent.emit();
   }
 
   delete(){
-    console.log("emit deletion " + this.type);
     this.deleteEvent.emit();
   }
 }
